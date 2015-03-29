3f9dc7d431c430ae4d5367e63f2d303e43097b04
Eivin Landa <landa@drivdigital.no>
2015-03-30 00:02:40 +0200

    Added a widgetarea for the footer

--- classes/class-pneumatic-theme-widget.php
+++ classes/class-pneumatic-theme-widget.php
@@ -7,7 +7,7 @@ class Pneumatic_Theme_Widget {
   /**
    * Register all widget areas used in the theme
    */
-  static function widget_area( $classes ) {
+  static function widget_area() {
     // Register sidebars
     register_sidebar( array(
       'name'          => __( 'Widget Area', 'pneumatic-theme' ),
@@ -15,6 +15,16 @@ class Pneumatic_Theme_Widget {
       'description'   => __( 'Add widgets here to appear in your sidebar.', 'pneumatic-theme' ),
       'before_widget' => '<aside id="%1$s" class="widget %2$s">',
       'after_widget'  => '</aside>',
+      'before_title'  => '<h2 class="widget-title">',
+      'after_title'   => '</h2>',
+    ) );
+
+    register_sidebar( array(
+      'name'          => __( 'Footer', 'pneumatic-theme' ),
+      'id'            => 'footer',
+      'description'   => __( 'Add widgets here to appear in your footer.', 'pneumatic-theme' ),
+      'before_widget' => '<div id="%1$s" class="widget %2$s">',
+      'after_widget'  => '</div>',
       'before_title'  => '<h2 class="widget-title">',
       'after_title'   => '</h2>',
     ) );
--- footer.php
+++ footer.php
@@ -1,5 +1,10 @@
 
 </div>
+<?php if ( is_active_sidebar( 'footer' ) ) : ?>
+  <footer class="footer widget-area" role="footer">
+    <?php dynamic_sidebar( 'footer' ); ?>
+  </footer>
+<?php endif; ?>
 <?php wp_footer(); ?>
 
 </body>
