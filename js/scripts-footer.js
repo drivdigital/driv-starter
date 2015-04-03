ab11e8517043c35906bdae3847ad3f084f25e406
Eivin Landa <landa@drivdigital.no>
2015-04-03 14:09:03 +0200

    Added a script to automatically highlight code in <pre> tags

--- js/scripts-footer.js
+++ js/scripts-footer.js
@@ -1,4 +1,26 @@
 jQuery( function( $ ) {
   // Fix attachments extending beyond the page border
   $( '.article--content div[style]' ).css( 'width', '' );
+
+
+  // Use highlight.js to hightlight code snippets
+  var code_queue = [];
+  $('pre').each( function() {
+    var text = $( this ).text();
+    if ( text.match( /[\{\[\;\[]|function/) ) {
+      code_queue.push( this );
+    }
+  } );
+
+  if ( code_queue.length ) {
+    $( '<link/>', {
+      rel: 'stylesheet',
+      href: '//cdnjs.cloudflare.com/ajax/libs/highlight.js/8.5/styles/solarized_light.min.css'
+    } ).appendTo( 'head' );
+    $.getScript( '//cdnjs.cloudflare.com/ajax/libs/highlight.js/8.5/highlight.min.js', function() {
+      $.each( code_queue, function( i, block ) {
+        hljs.highlightBlock( hljs.fixMarkup( block ) );
+      } );
+    } );
+  }
 } );
