4be034d7da8225d93c450d19635870951d33f268
Andrew Lazarus <yratof@gmail.com>
2015-10-27 09:21:55 +0100

    Updated Sass files, grunt process includes IE fixes

--- gruntfile.js
+++ gruntfile.js
@@ -1,9 +1,9 @@
 module.exports = function(grunt) {
- 
+
   grunt.registerTask('watch', [ 'watch' ]);
-  
+
   grunt.initConfig({
-    
+
     // Make JS tiny
     uglify: {
       options: {
@@ -24,22 +24,44 @@ module.exports = function(grunt) {
           expand: true,
           cwd: 'assets/images',
           src: ['**/*.{png,jpg,gif,svg}'],
-          dest: 'assets/img'
+          dest: 'assets/images'
         }]
+      }
+    },
+
+    // Import whole folder into a file 
+    sass_globbing: {
+      target: {
+        files: {
+          'assets/scss/partials/_mixins.scss': 'assets/scss/partials/mixins/*.scss',
+          'assets/scss/partials/_components.scss': 'assets/scss/components/*.scss'
+        }
       }
     },
     
     // Compile SCSS
 		sass: {
+      stage: {     
+        options: { 
+          style: 'nested', 
+          //loadPath: require('node-bourbon').includePaths
+        },   
+        files: {
+          'assets/css/style.css' : 'assets/scss/style.scss',
+          'assets/css/woocommerce.css' : 'assets/scss/woocommerce.scss',
+          'assets/css/login.css' : 'assets/scss/login.scss'
+        }
+      },
+
 			dist: {     
         options: { 
-          style: 'compressed',
-          loadPath: require('node-bourbon').includePaths
+          style: 'compressed', 
+          //loadPath: require('node-bourbon').includePaths
         },   
 				files: {
-					'assets/css/style.css' : 'assets/scss/style.scss',
-					'assets/css/ie.css' : 'assets/scss/ie.scss',
-					'assets/css/login.css' : 'assets/scss/login.scss'
+          'assets/css/style.css' : 'assets/scss/style.scss',
+          'assets/css/woocommerce.css' : 'assets/scss/woocommerce.scss',
+          'assets/css/login.css' : 'assets/scss/login.scss'
 				}
 			}
 		},
@@ -48,27 +70,11 @@ module.exports = function(grunt) {
     combine_mq: {
       target: {
         files: {
+          'assets/css/woocommerce.css': ['assets/css/woocommerce.css'],
           'assets/css/style.css': ['assets/css/style.css']
         },
         options: {
           beautify: false
-        }
-      }
-    },
-
-    // See your changes in the browser as they happen.
-    browserSync: {
-      default_options: {
-        bsFiles: {
-          src: [
-            "assets/css/*.css",
-            "*.php,",
-            "**/*.php,"
-          ]
-        },
-        options: {
-          watchTask: true,
-          proxy: "project.local" // If your site runs through MAMP, whats the URL
         }
       }
     },
@@ -85,19 +91,42 @@ module.exports = function(grunt) {
             remove: false
         },
         dist: {
-            src: 'css/*.css'
+            src: 'assets/css/*.css'
         }
     },
-    
-    // Import whole folder into a file 
-    sass_globbing: {
-      target: {
-        files: {
-          'assets/scss/components.scss': 'assets/scss/components/*.scss'
-        }
-      }
-    },
-    
+
+                /*
+                * Fallback for Internet Explorer 
+                */
+
+                pixrem: {
+                  options: {
+                    rootvalue: '100%',
+                    replace: true
+                  },
+                  dist: {
+                    src: 'assets/css/style.css',
+                    dest: 'assets/css/ie.css'
+                  }
+                },
+
+                stripmq: {
+                  options: {
+                    width: 1400,
+                    type: 'screen'
+                  },
+                  all: {
+                    files: {
+                      'assets/css/ie.css': ['assets/css/ie.css']
+                    }
+                  }
+                },
+
+
+                /*
+                * End of Fallback for Internet Explorer 
+                */
+
     // Watch for any changes
     watch: {
       js: {
@@ -105,12 +134,39 @@ module.exports = function(grunt) {
         tasks: ['uglify:js']
       },
       css: {
-        // Watch sass changes, merge mqs & run bs
-        files: ['assets/scss/*.scss', 'assets/scss/**/*.scss'],
-        tasks: ['sass_globbing:target', 'sass', 'combine_mq:target', 'postcss:dist', 'browserSync' ]
+        // Watch sass changes, then process new images and merge mqs
+        files: [
+        'assets/scss/*.scss', 
+        'assets/scss/**/*.scss',
+        'assets/scss/**/**/*.scss'],
+        tasks: [
+          'sass_globbing:target', 
+          'sass:stage', 
+          'postcss:dist', 
+          'pixrem',
+          'stripmq',
+          'browserSync'
+        ]
       },
       options: {
-          spawn: false // Very important, don't miss this
+        spawn: false // Very important, don't miss this
+      }
+    },
+
+    browserSync: {
+      default_options: {
+        bsFiles: {
+          src: [
+            "assets/css/*.css",
+            "*.html,",
+            "**/*.html,",
+            "*.php,",
+            "**/*.php,"
+          ]
+        },
+        options: {
+          watchTask: true,
+        }
       }
     }
   });
@@ -121,14 +177,25 @@ module.exports = function(grunt) {
   // Run everything with 'grunt', these need to be in
   // a specific order so they don't fail.
   grunt.registerTask('default', [
-    'uglify', 
     'sass_globbing:target', // Glob together needed folders
-    'sass', // Run sass
-    'combine_mq', // Combine MQ's
+    'sass:stage', // Run sass
     'postcss:dist', // Post Process with Auto-Prefix
-    'newer:imagemin:dynamic', // Compress all images
     'browserSync', // live reload
+    'newer:imagemin:dynamic', // Compress all images
+    'pixrem', // Fallback for IE and Android
+    'stripmq',
     'watch' // Keep watching for any changes
   ]);
 
+  // When we go live, run `grunt live` instead
+  grunt.registerTask('live', [
+    'uglify', 
+    'sass_globbing:target', // Glob together needed folders
+    'sass:dist', // Run sass
+    'postcss:dist', // Post Process with Auto-Prefix
+    'combine_mq', // Combine MQ's
+    'newer:imagemin:dynamic', // Compress all images
+    'pixrem', // Fallback for IE and Android
+    'stripmq' // Take it all away.
+  ]);
 };
