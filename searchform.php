8cfc3be8f588320ad64373b5497a9d189cb4e6c3
Andrew Lazarus <yratof@gmail.com>
2016-03-21 14:23:18 +0100

    github pages is simply readme file

--- searchform.php
+++
@@ -1,7 +0,0 @@
-<form role="search" method="get" id="searchform" class="searchform" action="<?php echo home_url( '/' ); ?>">
-    <div class="searchform--inner cf">
-        <label for="s" class="screen-reader-text"><?php _e('Search for:','drivdigital'); ?></label>
-        <input type="search" id="s" name="s" value="" placeholder="" />
-        <input type="submit" id="searchsubmit" value="<?php _e('Search','drivdigital'); ?>">
-    </div>
-</form>
